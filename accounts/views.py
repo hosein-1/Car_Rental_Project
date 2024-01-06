ea5b34cb2540d98d4db05424832ad3a298091fcc
hosein-1 <hoseinlaki16@gmail.com>
2024-01-06 22:02:39 +0330

    implement Signu Up

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    
+    else:
+        form = UserCreationForm()
+    
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'registration/signup.html', context)
+
+
