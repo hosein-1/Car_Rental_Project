4a7eeedc67200886b40156a9373fb04bb25e039a
hosein-1 <hoseinlaki16@gmail.com>
2024-01-06 19:21:06 +0330

    add CustomUser model and config

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-
-
+class CustomUser(AbstractUser):
+    national_code = models.CharField(max_length=20)
+    phone_number = models.CharField(max_lenght=20)
+    address = models.CharField(max_length=500)
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 
 
 ]
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom User Config
+AUTH_USER_MODEL = "accounts.CustomUser"
