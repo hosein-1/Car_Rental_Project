777e0dfc42ce3337c998b9d3860a6a97d2896ede
hosein-1 <hoseinlaki16@gmail.com>
2024-01-21 19:01:54 +0330

    create category and car app

--- cars/models.py
+++ cars/models.py
@@ -4,3 +4,17 @@ from django.conf import settings
 
 class Customer(models.Model):
     pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField()
+
+
+class Car(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=255)
+    model = models.CharField(max_length=500)
+    number_plate = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    price = models.PositiveIntegerField(blank=True)
