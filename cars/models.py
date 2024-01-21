2ebc25897e6d7cd962b3b6dc1f8744fc63aff4dc
hosein-1 <hoseinlaki16@gmail.com>
2024-01-21 20:50:20 +0330

    create reservation model

--- cars/models.py
+++ cars/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django_jalali.db import models as jmodels
 
 
 class Customer(models.Model):
@@ -35,5 +36,23 @@ class Driver(models.Model):
     certificate_base = models.CharField(max_length=255)
 
     def __str__(self):
-        return f'{self.driver.first_name} {self.driver.last_name}'
+        return f'{self.user.first_name} {self.user.last_name}'
 
+
+class Reservation(models.Model):
+    PAID = 'p'
+    UNPAID = 'up'
+    PAYMENT_STATUS = [
+        (PAID, 'paid'),
+        (UNPAID, 'unpaid')
+    ]
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    car = models.ForeignKey(Car, on_delete=models.PROTECT)
+    driver = models.ForeignKey(Driver, on_delete=models.PROTECT)
+    start_date = jmodels.jDateTimeField(auto_now_add=True)
+    end_date = jmodels.jDateTimeField(auto_now_add=True)
+    delivery_address = models.CharField(max_length=500)
+    is_paid = models.CharField(max_length=6, choices=PAYMENT_STATUS, default=PAID)
+
+    def __str__(self):
+        return f'{self.customer} : {self.car}'
