a2e162b9856b24108dc34e062a5ec0de7c5bc5ac
hosein-1 <hoseinlaki16@gmail.com>
2024-01-29 12:50:59 +0330

    add customer model to admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['user', ]
+
