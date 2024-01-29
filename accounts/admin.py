c34d5958b024c09f6d555b7e626e123a85a67710
hosein-1 <hoseinlaki16@gmail.com>
2024-01-29 12:38:41 +0330

    add customuseradmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,18 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'user_type']
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age', 'user_type', 'phone_number', 'address', 'national_code')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age', 'user_type', 'phone_number', 'address', 'national_code')}),
+    )
+
+
