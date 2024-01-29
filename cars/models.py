e9770005cc0f1e5a7e57c4f0d2a1306beb6ecade
hosein-1 <hoseinlaki16@gmail.com>
2024-01-29 12:53:30 +0330

    add verbose_name for category model

--- cars/models.py
+++ cars/models.py
@@ -18,6 +18,10 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     price = models.PositiveIntegerField()
 
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی'
+
     def __str__(self):
         return f'{self.name}'
 
