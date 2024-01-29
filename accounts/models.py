c49328bbc0cd4b5dc4fdf96cfcb322248eadef4a
hosein-1 <hoseinlaki16@gmail.com>
2024-01-29 12:08:22 +0330

    add user_type for customuser model

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,18 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
+    CUSTOMER = 'c'
+    DRIVER = 'd'
+    ADMIN = 'a'
+    USER_STATUS = [
+        (CUSTOMER, 'customer'),
+        (DRIVER, 'driver'),
+        (ADMIN, 'admin')
+    ]
     national_code = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=500)
     age = models.PositiveIntegerField(default=None)
+    user_type = models.CharField(max_length=1, choices=USER_STATUS, default=CUSTOMER)
 
 
