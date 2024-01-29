73f76322c3180f98253d32ef36687a4da4b8bf59
hosein-1 <hoseinlaki16@gmail.com>
2024-01-29 13:39:15 +0330

    add verbose_name for page model

--- pages/models.py
+++ pages/models.py
@@ -7,6 +7,13 @@ class Page(models.Model):
     body = models.TextField()
     is_active = models.BooleanField(default=True)
 
+    class Meta:
+        verbose_name = 'صفحه'
+        verbose_name_plural = 'صفحه'
+
+    def __str__(self):
+        return f'{self.title}'
+
 
 class Questions(models.Model):
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
