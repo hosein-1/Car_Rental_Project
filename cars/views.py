a12673057dc947f66fc8b4a4d38fdb30ff6fb4fd
hosein-1 <hoseinlaki16@gmail.com>
2024-01-30 17:40:32 +0330

    implement search view

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 from .models import Car, Customer, Reservation
 from .forms import ReservationForm
@@ -11,7 +12,6 @@ def home(request):
         'cars': cars,
     }
     return render(request, 'cars/home.html', context)
-
 
 
 def cars_list(request):
@@ -56,4 +56,15 @@ def user_profile(request):
     return render(request, 'profile.html', context)
 
 
+def search(request):
+    query = request.GET.get('q')
 
+    if query:
+        object_list = Car.objects.filter(
+            Q(name__icontains=query) | Q(category__name__icontains=query) | Q(model__icontains=query)
+        )
+
+    context = {
+        'object_list': object_list,
+    }
+    return render(request, 'cars/search_list.html', context)
