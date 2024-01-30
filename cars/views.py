fcaddbfce72dc70a9c7e3140c7de99c4242650e2
hosein-1 <hoseinlaki16@gmail.com>
2024-01-30 11:56:17 +0330

    add cars_list view

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+from .models import Car
+
+
+def cars_list(request):
+    cars = Car.objects.all()
+    context = {
+        'cars': cars,
+    }
+    return render(request, 'cars/cars_list.html', context)
+
+
--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('cars.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
 ]
