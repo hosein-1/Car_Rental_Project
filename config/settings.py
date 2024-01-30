38e91b79db8b9e4289f9c3f3c833637cea4f2c58
m-jalali-25-36 <m.jalali.25.36@gmail.com>
2024-01-30 14:36:04 +0330

    add setting static file in config/setting.py

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATIC_URL = 'static/'
 
 # Default primary key field type
