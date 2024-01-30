25141af5980f5f08e8e99c405ebb9e99372bc382
m-jalali-25-36 <m.jalali.25.36@gmail.com>
2024-01-30 14:40:03 +0330

    added home view

--- cars/urls.py
+++ cars/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('cars/', views.cars_list, name='cars_list'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render, HttpResponse
 from .models import Car
 
 
+def home(request):
+    cars = Car.objects.all()[:6]
+    context = {
+        'cars': cars,
+    }
+    return render(request, 'cars/home.html', context)
+
+
+
 def cars_list(request):
     cars = Car.objects.all()
     context = {
