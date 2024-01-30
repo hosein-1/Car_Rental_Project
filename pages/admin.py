eb109fffc9f251b0d365a7a64793ace90182f393
hosein-1 <hoseinlaki16@gmail.com>
2024-01-30 22:06:25 +0330

    add questions model to admin

--- pages/admin.py
+++ pages/admin.py
@@ -7,3 +7,8 @@ from . import models
 class PageAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'is_active']
     list_editable = ['is_active']
+
+
+@admin.register(models.Questions)
+class QuestionsAdmin(admin.ModelAdmin):
+    list_display = ['author', 'parent']
