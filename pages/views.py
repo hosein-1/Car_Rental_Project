c03fa41aee4369d019c721bf2bccd5099bcc456f
m-jalali-25-36 <m.jalali.25.36@gmail.com>
2024-01-30 22:33:37 +0330

    added page view and template

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Page
 
 # Create your views here.
+def page(request, slug):
+    page = get_object_or_404(Page, slug=slug)
+
+    if(not page.is_active):
+        #not find
+        
+        b = 0
+
+    context = {
+        'page': page,
+    }
+    return render(request, 'pages/page.html', context)
