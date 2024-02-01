e45fb06c621e30bfcb53fa5a482047ebc0c47ed5
hosein-1 <hoseinlaki16@gmail.com>
2024-02-01 17:31:18 +0330

    set blank=true for address

--- accounts/models.py
+++ accounts/models.py
@@ -16,7 +16,7 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(verbose_name='نام خانوادگی', max_length=255)
     national_code = models.CharField(verbose_name='کد ملی', max_length=20)
     phone_number = models.CharField(verbose_name='شماره تلفن', max_length=20)
-    address = models.CharField(max_length=500)
+    address = models.CharField(max_length=500, blank=True)
     age = models.PositiveIntegerField(verbose_name='سن', blank=True, null=True)
 
 
